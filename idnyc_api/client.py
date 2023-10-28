8a085cf502727ae02a416bbd24fd32dd886a73c9
Jacob Padilla <jp@jacobpadilla.com>
2023-10-28 17:36:41 -0400

    almost done with the refactoring!

--- idnyc_api/client.py
+++ idnyc_api/client.py
@@ -1,24 +1,93 @@
 import random
 import asyncio
 import httpx
+from datetime import datetime
+import random
 from fake_useragent import UserAgent
 
 from typing import Any
 
 class AsyncClient:
-    def __init__(self):
-        self.user_agent = UserAgent().random
+    def __init__(self, *, user_agent: str = None, httpx_async_client: httpx.AsyncClient = None, proxy: str = None):
+        self.user_agent = user_agent or UserAgent().random
         self.browser_token = self.generate_random_string()
+        self.http_client: httpx_async_client or httpx.AsyncClient
+        self.proxy = proxy
 
         self.user_token: str
-        self.http_client: httpx.AsyncClient
+        
+    async def check_availability_range(
+        self, start_date: datetime, 
+        end_date: datetime, 
+        *, 
+        request_rest: int = None
+    ) -> dict[str, Any]:
+
+        all_boroughs = ['3', '2', '4', '5', '1']
+        all_locations = ['3201', '3298', '3297', '3300', '3150', '3253', '3289', '3293']
+
+        current_date = start_date
+        while current_date <= end_date:
+            print('\033[38;5;69m' + f'{current_date.strftime("%m/%d/%Y") :-<150}' + '\033[0m')
+
+            string_date = current_date.strftime('%m/%d/%Y')
+            one_day = await asyncio.gather(
+                self._fetch_data('Morning', all_locations, all_boroughs, string_date),
+                self._fetch_data('Afternoon', all_locations, all_boroughs, string_date),
+                self._fetch_data('Evening', all_locations, all_boroughs, string_date),
+                return_exceptions=True
+            )
+
+            for task_resp in one_day:
+                if isinstance(task_resp, Exception):
+                    print(task_resp)
+                    asyncio.sleep(3)
+
+                else:
+                    if self.check_if_there_is_availability(str(task_resp)):
+                        print(task_resp)
+                    else:
+                        print("No Availability")
+
+            current_date += datetime.timedelta(days=1) 
+           
+            if request_rest is not None:
+                await asyncio.sleep(random.uniform(request_rest - request_rest / 2, request_rest + request_rest / 2))
+
+    async def check_availability_day( self, date: datetime) -> dict[str, Any]:
+        all_boroughs = ['3', '2', '4', '5', '1']
+        all_locations = ['3201', '3298', '3297', '3300', '3150', '3253', '3289', '3293']
+
+        string_date = date.strftime('%m/%d/%Y')
+        one_day = await asyncio.gather(
+            self._fetch_data('Morning', all_locations, all_boroughs, string_date),
+            self._fetch_data('Afternoon', all_locations, all_boroughs, string_date),
+            self._fetch_data('Evening', all_locations, all_boroughs, string_date),
+            return_exceptions=True
+        )
+
+        for task_resp in one_day:
+            if isinstance(task_resp, Exception):
+                print(task_resp)
+                asyncio.sleep(3)
+
+            else:
+                if self.check_if_there_is_availability(str(task_resp)):
+                    print(task_resp)
+                else:
+                    print("No Availability")
 
     async def __aenter__(self):
-        self.http_client = httpx.AsyncClient()
+        self.http_client = httpx.AsyncClient(proxies=self.proxy)
+
+        # Initialize session token
+        await self.set_user_token()
+        await asyncio.sleep(random.randint(3,5))  # Rest
+
         return self
 
     async def __aexit__(self, exc_type, exc_value, traceback):
-        await self.http_client.close()
+        await self.http_client.aclose()
 
     @staticmethod
     def generate_random_string(length: int = 32) -> str:
@@ -49,9 +118,6 @@ class AsyncClient:
         data = response.json()
         self.user_token = data['userToken']
 
-    async def check_availability(self, start_date, end_date, *, speed: int = 1) -> dict:
-        pass
-
     async def _fetch_data(self, start_time: str, locations: list[str], boroughs: list[str], date: str) -> dict[str, Any]:
         assert self.user_token, 'User token is not set.'
 
@@ -77,7 +143,7 @@ class AsyncClient:
 
         url = 'https://a069-idnyconlineportal.nyc.gov/IOPWebServices/api/AppointmentApi/GetAvailableTimeSlots' 
 
-        response = await self.http_client.post( url, headers=headers, json=data)
+        response = await self.http_client.post(url, headers=headers, json=data)
 
         if response.status_code != 200:
             raise HttpRequestError(response.status_code)
@@ -91,7 +157,6 @@ class AsyncClient:
         you can assume that slots are available.
         """
         no_slots = "{'$id': '1', 'responseStatusDto': {'$id': '2', 'type': 1, 'errors': {'$id': '3', '$values': []}}, 'responseDataDto': {'$id': '4', 'data': {'$id': '5', '$values': []}}}"
-
         return response != no_slots
     
 
--- test.py
+++ test.py
@@ -1,4 +1,11 @@
+import asyncio
+import datetime
 from idnyc_api import AsyncClient
 
-async with AsyncClient as client:
-    client.
+async def main():
+    async with AsyncClient() as client:
+        date = datetime.date(2023, 10, 29)
+        await client.check_availability_day(date)
+
+if __name__ == '__main__':
+    asyncio.run(main())
